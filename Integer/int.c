73318f71ea5a129f4a31a72c0dbe1148e8264c68
SebasFranco94 <franco_9408@hotmail.com>
2020-06-22 18:53:35 -0500

    Programa para calcular los dias de edad de una persona

--- Integer/int.c
+++ Integer/int.c
@@ -1,12 +1,11 @@
-#include <cs50.h>
 #include <stdio.h>
 
 int main(void) {
   /* code */
-    /*char answer[10];
-    printf("What's your name?\n");
-    scanf("%s",answer);
-    printf("Hello %s\n", answer);*/
-    string answer = get_string("What's your name?\n");
-    printf("Hello, %s\n", answer);
+  int age;
+  int days;
+  printf("What's your age?\n");
+  scanf("%i", &age);
+  days= 365*age;
+  printf("You are at least %i days old\n",days);
 }
