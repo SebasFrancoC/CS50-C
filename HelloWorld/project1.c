b3ba53286c13d4a7dccf8309ad7a764a3b1a3d78
SebasFranco94 <franco_9408@hotmail.com>
2020-06-22 15:38:49 -0500

    Cree una carperta he introduje todos los archivos del project1 (HelloWorld)

--- HelloWorld/project1.c
+++ HelloWorld/project1.c
@@ -3,8 +3,9 @@
 int main(void) {
   /* code */
   char name[10];
+  char lastName[10];
   printf("Hello, what's your name?\n");
-  scanf("%s",name );
-  printf("Hello %s\n",name);
+  scanf("%s %s",name, lastName);
+  printf("Hello %s %s\n",name, lastName);
   return 0;
 }
