c230485607f63a61ad6c867dbb022d5b7a540a11
SebasFranco94 <franco_9408@hotmail.com>
2020-06-23 11:41:45 -0500

    Creación de función cough

--- Cough/Cough2/cough2.c
+++ Cough/Cough2/cough2.c
@@ -1,11 +1,15 @@
 //Cough2
 #include <stdio.h>
 
+void cough(void) {
+  /* code */
+  printf("Cough\n");
+}
 int main(void) {
   /* code */
   for (size_t i = 0; i < 3; i++) {
     /* code */
-    printf("Cough\n");
+    cough();
   }
 
 }
