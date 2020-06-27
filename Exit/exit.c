edba2c621799de99b2402ffc4ddf2e1a1f1456d1
SebasFranco94 <franco_9408@hotmail.com>
2020-06-27 15:05:43 -0500

    Cree carpeta Exit y archivo exit.c

---
+++ Exit/exit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        //Wrong
+        printf("Missing command-line argument\n");
+        return 1;
+    }
+    // put something $ ./argv sebastian
+    printf("Hello %s\n",argv[1]);
+    return 0;
+}
